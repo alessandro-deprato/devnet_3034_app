5aa3a692ca06506efe3fa317a6736f958ffbb30c
adeprato <adeprato@cisco.com>
2022-04-24 13:41:09 +0200

    adding a third app

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "helm" {
 
 resource "helm_release" "helloiksapp" {
   name       = "helloiksapp"
-  namespace        = "iks-adp-namespace-2"
+  namespace        = "iks-adp-namespace"
   create_namespace = true
   chart = "https://github.com/alessandro-deprato/devnet_3034_app/raw/master/helloiks_adp-0.1.1.tgz"
   set {
