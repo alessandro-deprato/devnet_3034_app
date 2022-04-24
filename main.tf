51ffa931452997a4b5cb166a5d559a1f424c1bb6
adeprato <adeprato@cisco.com>
2022-04-24 15:30:03 +0200

    changed the new chart path

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "helm_release" "helloiksapp" {
   name       = "helloiksapp"
   namespace        = "iks-adp-namespace"
   create_namespace = true
-  chart = "https://github.com/alessandro-deprato/devnet_3034_app/raw/master/helloiks_adp-0.1.0.tgz"
+  chart = "https://github.com/alessandro-deprato/devnet_3034_app/raw/master/helloiks-adp-0.1.2.tgz"
   set {
     name  = "MESSAGE"
     value = "Hello IKS from TFCB!!"
